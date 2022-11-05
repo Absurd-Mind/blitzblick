0580363f70e61a9449d30f1103104a5a18d990f4
Peter Kranz <peter@myref.net>
2022-11-05 17:06:43 +0100

    add Fiverfield, remove scaffolding

---
+++ src/components/FiverField.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+export interface FeldProps {
+  filled: number;
+}
+
+const props = defineProps<FeldProps>();
+</script>
+
+<template>
+  <div class="feld">
+    <span v-for="i in props.filled" v-bind:key="i">⬤</span>
+  </div>
+</template>
+
+<style scoped>
+.feld {
+  border-color: black;
+  border-width: 1px;
+  border-style: solid;
+}
+</style>
---
+++ src/components/__tests__/FiverField.spec.ts
@@ -0,0 +1,18 @@
+import { describe, it, expect, test } from "vitest";
+
+import { mount } from "@vue/test-utils";
+import FiverField from "../FiverField.vue";
+
+describe("FiverField", () => {
+  test.each([
+    [0, ""],
+    [1, "⬤"],
+    [2, "⬤⬤"],
+    [3, "⬤⬤⬤"],
+    [4, "⬤⬤⬤⬤"],
+    [5, "⬤⬤⬤⬤⬤"],
+  ])("filled(%i) -> %s", (filled, expected) => {
+    const wrapper = mount(FiverField, { props: { filled: filled } });
+    expect(wrapper.text()).toBe(expected);
+  });
+});
